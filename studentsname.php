61c32b6716dc449336ac9d8d08939b5f7aefb021
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-01-24 11:56:10 +0530

    Add files via upload

---
+++ studentsname.php
@@ -0,0 +1,18 @@
+<?php
+$arr=["rahul","aswin","john","balu"];
+$t=$arr;
+
+echo "name sorting";
+
+echo "<br> <br>";
+echo "display using print_r() <br>";
+print_r($t);
+echo "<br> <br>";
+echo "sorting using asort() <br>";
+asort($t);
+print_r($t);
+echo "<br> <br>";
+echo "sorting using arsort() <br>";
+arsort($t);
+print_r($t);
+?>
